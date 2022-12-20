64fecd2c9797f9c1a06d00e89fef89de861fb1ab
Sara <sara@studiopresent.com>
2022-12-20 09:50:08 +0100

    20.12.2022. update

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,18 @@
 let scrollpos = window.scrollY;
 
-const header = document.getElementsByClassName("header");
+const header = document.querySelector(".header");
+const dropdown = document.querySelector(".dropdown-content")
 const scrollChange = 1;
 console.log(header);
 
-const add_class_on_scroll = () => header.classList.add("bg-red");
-const remove_class_on_scroll = () => header.classList.remove("bg-red");
+const add_class_on_scroll = () => {
+    header.classList.add("header-color");
+    dropdown.classList.add("dropdown-color");
+}
+const remove_class_on_scroll = () => {
+    header.classList.remove("header-color");
+    dropdown.classList.remove("dropdown-color");
+}
 
 window.addEventListener('scroll', function() {
     scrollpos = window.scrollY;
